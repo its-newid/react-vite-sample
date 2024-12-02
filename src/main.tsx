ed219a89f077a0c7b22fa925898935e7390a6727
minho-newid <minho@its-newid.com>
2024-12-02 16:39:30 +0900

    🔥 Remove app.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { Router as Navigation } from '@/routes';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
@@ -8,8 +8,8 @@ import { theme } from './styles/theme';
 createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <BrowserRouter>
+        <HashRouter>
             <Navigation />
-        </BrowserRouter>
+        </HashRouter>
     </ThemeProvider>,
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react';
 import styled from 'styled-components';
+import { useAtom } from 'jotai';
 import reactLogo from '@/assets/react.svg';
 import viteLogo from '@/assets/vite.svg';
-import '@/styles/App.css';
+import { countAtom } from '@/store/app';
 
 const HomePage = () => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useAtom(countAtom);
 
     return (
         <Container>
             <div>
                 <a href='https://vite.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
+                    <Logo src={viteLogo} className='logo' alt='Vite logo' />
                 </a>
                 <a href='https://react.dev' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
+                    <SpinLogo src={reactLogo} alt='React logo' />
                 </a>
             </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
+            <h1>Vite + React + Jotai + Styled-Components</h1>
+            <button onClick={() => setCount((count) => count + 1)}>+ 1</button>
+            <div>count is {count}</div>
             <p className='read-the-docs'>
                 Click on the Vite and React logos to learn more
             </p>
@@ -39,4 +29,29 @@ const HomePage = () => {
 
 export default HomePage;
 
-const Container = styled.div``;
+const Container = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Logo = styled.img`
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+    &:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+    }
+`;
+
+const SpinLogo = styled(Logo)`
+    animation: logo-spin infinite 20s linear;
+    &:hover {
+        filter: drop-shadow(0 0 2em #61dafbaa);
+    }
+`;
